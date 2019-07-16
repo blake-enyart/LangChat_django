9e4c5eeed26d232d0c45d59c7e388e79b55bd062
Blake <blake.enyart@gmail.com>
2019-07-15 20:19:09 -0600

    Chat application code-along, will use as template

--- chat_app/models/message.py
+++ chat_app/models/message.py
@@ -1,7 +1,12 @@
 from django.db import models
+from .room import Room
+from django.utils import timezone
 
 class Message(models.Model):
-    room = models.ForeignKey(Room, related_name='messages')
+    room = models.ForeignKey(Room,
+        related_name='messages',
+        on_delete=models.CASCADE,
+    )
     handle = models.TextField()
     message = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
