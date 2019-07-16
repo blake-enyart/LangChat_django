aefd1e3662107942e072e52b584059fbbfaaeeef
Blake <blake.enyart@gmail.com>
2019-07-15 20:30:14 -0600

    Add django-heroku

--- LangChat/settings.py
+++ LangChat/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 import environ
 
@@ -156,3 +156,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
