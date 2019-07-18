081b299d06378e7e287fa1c1b2760c0bc9984f81
Blake <blake.enyart@gmail.com>
2019-07-17 20:54:05 -0600

    pg db integrations for heroku

--- LangChat/settings.py
+++ LangChat/settings.py
@@ -178,4 +178,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 django_heroku.settings(locals())
