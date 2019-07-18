2a2c74d521d349f59e779a9c37c712582a780e5f
Blake <blake.enyart@gmail.com>
2019-07-18 16:36:40 -0600

    WIP: Travis CI rebuild

--- LangChat/settings/test.py
+++ LangChat/settings/test.py
@@ -5,7 +5,7 @@ SECRET_KEY = env('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql_psycopg2',
-        'NAME':     'travisci',
+        'NAME':     'travis_ci_test',
         'USER':     'postgres',
         'PASSWORD': '',
         'HOST':     'localhost',
