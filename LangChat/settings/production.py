d86f8ea7a5f6f9c82a89e5a43ab2144de5da80c1
Blake <blake.enyart@gmail.com>
2019-07-18 22:58:50 -0600

    PG conflict test for heroku

--- LangChat/settings/production.py
+++ LangChat/settings/production.py
@@ -4,16 +4,9 @@ DEBUG = env('DEBUG')
 
 SECRET_KEY = env('SECRET_KEY')
 
-DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASS'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+DATABASE_URL = env('DATABASE_URL')
+
+DATABASES = {}
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
