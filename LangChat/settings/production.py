255e0612839536274b0b97319191a90401971327
Blake <blake.enyart@gmail.com>
2019-07-18 23:49:02 -0600

    attempt 20

--- LangChat/settings/production.py
+++ LangChat/settings/production.py
@@ -10,5 +10,5 @@ django_heroku.settings(locals())
 
 import dj_database_url
 DATABASES = {
-'default': dj_database_url.config(default=env('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
