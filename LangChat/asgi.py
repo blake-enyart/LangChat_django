26051264859a03c032bd50bdde9bf2b1a75cac41
Blake <blake.enyart@gmail.com>
2019-07-18 21:26:09 -0600

    WIP: binding DJANGO_SETTINGS_MODULE in ASGI and WSGI files

--- LangChat/asgi.py
+++ LangChat/asgi.py
@@ -7,5 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'LangChat.settings.production'
 django.setup()
 application = get_default_application()
