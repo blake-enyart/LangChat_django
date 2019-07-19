dd455355af508b25cdbc8b8caa3f5608b8b86433
Blake <blake.enyart@gmail.com>
2019-07-18 23:47:34 -0600

    Move db setup to very bottom

--- LangChat/settings/production.py
+++ LangChat/settings/production.py
@@ -4,11 +4,11 @@ DEBUG = env('DEBUG')
 
 SECRET_KEY = env('SECRET_KEY')
 
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(default=env('DATABASE_URL'), conn_max_age=600, ssl_require=True)
-}
-
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES = {
+'default': dj_database_url.config(default=env('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+}
