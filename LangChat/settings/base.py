83238517ca0a81b83af2c27ff7c90efa11b99d2f
Blake <blake.enyart@gmail.com>
2019-07-19 18:10:09 -0600

    Just need something to work in this authentication cycle

--- LangChat/settings/base.py
+++ LangChat/settings/base.py
@@ -145,7 +145,6 @@ CHANNEL_LAYERS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
 }
